edcd2903dbd13e9fe18050a3a05f41f41a7dc711
ValgardurRI <valgardur16@ru.is>
2017-10-14 18:58:42 +0000

    added ability to use both \n and commas in add() method

--- StringCalculator.java
+++ StringCalculator.java
@@ -2,7 +2,8 @@ public class StringCalculator
 {
 	public static int add(String numbers)
 	{
-		String[] splitNums = numbers.split(",");
+		String modifiedNumbers = numbers.replace('\n', ',');
+		String[] splitNums = modifiedNumbers.split(",");
 		int sum = 0;
 		for(int i = 0; i<splitNums.length; i++)
 		{
@@ -29,11 +30,14 @@ public class StringCalculator
 		System.out.println("Generating string s1: \"" + s5 + "\"");
 		String s6 = "5,,3,2";
 		System.out.println("Generating string s1: \"" + s6 + "\"");
+		String s7 = "1\n2,3";
+		System.out.println("Generating string s1: \"" + s6 + "\"");
 		System.out.println("Testing \"" + s1 + "\" == 0 " + (add(s1) == 0));
 		System.out.println("Testing \"" + s2 + "\" == 1 " + (add(s2) == 1));
 		System.out.println("Testing \"" + s3 + "\" == 17 " + (add(s3) == 17));
 		System.out.println("Testing \"" + s4 + "\"== 40 " + (add(s4) == 40));
 		System.out.println("Testing \"" + s5 + "\"== 10 " + (add(s5) == 10));
 		System.out.println("Testing \"" + s6 + "\" == 10 " + (add(s6) == 10));
+		System.out.println("Testing \"" + s7 + "\" == 6 " + (add(s7) == 6));
 	}
 }
