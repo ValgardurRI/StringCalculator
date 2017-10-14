5901be86e6c043ff43675344c07f42be9d05f69c
ValgardurRI <valgardur16@ru.is>
2017-10-14 18:14:02 +0000

    added the possibility to calculate the sum of an unknown amount of numbers

--- StringCalculator.java
+++ StringCalculator.java
@@ -13,7 +13,12 @@ public class StringCalculator
 		else
 		{
 			String[] splitNums = numbers.split(",");
-			return Integer.parseInt(splitNums[0]) + Integer.parseInt(splitNums[1]);
+			int sum = 0;
+			for(int i = 0; i<splitNums.length; i++)
+			{
+				sum += Integer.parseInt(splitNums[i]);
+			}
+			return sum;
 		}
 
 	}
@@ -23,8 +28,12 @@ public class StringCalculator
 		String s1 = "";
 		String s2 = "1";
 		String s3 = "8,9";
+		String s4 = "10,10,10,10";
+		String s5 = "1,1,1,1,1,1,1,1,1,1";
 		System.out.println(add(s1) == 0);
 		System.out.println(add(s2) == 1);
 		System.out.println(add(s3) == 17);
+		System.out.println(add(s4) == 40);
+		System.out.println(add(s5) == 10);
 	}
 }
