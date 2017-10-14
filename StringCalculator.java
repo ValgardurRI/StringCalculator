aa92d4b79cadb2e079aa4158a6984129e40061fd
ValgardurRI <valgardur16@ru.is>
2017-10-14 18:37:56 +0000

    added a test case where a number can be empty, fixed code to accept such a string, refactored add() to look more clean, refactored main() to be more descriptive

--- StringCalculator.java
+++ StringCalculator.java
@@ -2,38 +2,38 @@ public class StringCalculator
 {
 	public static int add(String numbers)
 	{
-		if(numbers.isEmpty())
+		String[] splitNums = numbers.split(",");
+		int sum = 0;
+		for(int i = 0; i<splitNums.length; i++)
 		{
-			return 0;
-		}
-		else if(!numbers.contains(","))
-		{
-			return Integer.parseInt(numbers);
-		}
-		else
-		{
-			String[] splitNums = numbers.split(",");
-			int sum = 0;
-			for(int i = 0; i<splitNums.length; i++)
+			if(splitNums[i].isEmpty())
 			{
-				sum += Integer.parseInt(splitNums[i]);
+				continue;
 			}
-			return sum;
+			sum += Integer.parseInt(splitNums[i]);
 		}
-
+		return sum;
 	}
 
 	public static void main(String[] args)
 	{
 		String s1 = "";
+		System.out.println("Generating string s1: \"" + s1 + "\"");
 		String s2 = "1";
+		System.out.println("Generating string s1: \"" + s2 + "\"");
 		String s3 = "8,9";
+		System.out.println("Generating string s1: \"" + s3 + "\"");
 		String s4 = "10,10,10,10";
+		System.out.println("Generating string s1: \"" + s4 + "\"");
 		String s5 = "1,1,1,1,1,1,1,1,1,1";
-		System.out.println(add(s1) == 0);
-		System.out.println(add(s2) == 1);
-		System.out.println(add(s3) == 17);
-		System.out.println(add(s4) == 40);
-		System.out.println(add(s5) == 10);
+		System.out.println("Generating string s1: \"" + s5 + "\"");
+		String s6 = "5,,3,2";
+		System.out.println("Generating string s1: \"" + s6 + "\"");
+		System.out.println("Testing \"" + s1 + "\" == 0 " + (add(s1) == 0));
+		System.out.println("Testing \"" + s2 + "\" == 1 " + (add(s2) == 1));
+		System.out.println("Testing \"" + s3 + "\" == 17 " + (add(s3) == 17));
+		System.out.println("Testing \"" + s4 + "\"== 40 " + (add(s4) == 40));
+		System.out.println("Testing \"" + s5 + "\"== 10 " + (add(s5) == 10));
+		System.out.println("Testing \"" + s6 + "\" == 10 " + (add(s6) == 10));
 	}
 }
