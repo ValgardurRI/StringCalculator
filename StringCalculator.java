b35700a5158a40033a3c138c01df58a886d84ee0
ValgardurRI <valgardur16@ru.is>
2017-10-15 19:52:59 +0000

    starting a string with //[char]\a adds it as a delimiter

--- StringCalculator.java
+++ StringCalculator.java
@@ -6,8 +6,16 @@ public class StringCalculator
 		{
 			return 0;
 		}
+		
+		String modifiedNumbers = numbers.replace('\n', ',');	
 
-		String modifiedNumbers = numbers.replace('\n', ',');
+		if(numbers.startsWith("//"))
+		{
+			char delimiter = numbers.charAt(2);
+			modifiedNumbers = modifiedNumbers.replace(delimiter, ',');
+			modifiedNumbers = modifiedNumbers.substring(4);
+		}
+
 		String[] splitNumbers = modifiedNumbers.split(",");
 		int sum = 0;
 		String invalidNumbers = "";
@@ -61,6 +69,10 @@ public class StringCalculator
 		System.out.println("Generating string s9: \"" + s9 + "\"");
 		String s10 = "1,10,100,1000,2000,9999";
 		System.out.println("Generating string s10: \"" + s10 + "\"");
+		String s11 = "//a\n1a2a3";
+		System.out.println("Generating string s11: \"" + s11 + "\"");
+		String s12 = "//;\n100;80;60;50";
+		System.out.println("Generating string s12: \"" + s12 + "\"");
 
 		System.out.println("Testing \"" + s1 + "\" == 0 " + (add(s1) == 0));
 		System.out.println("Testing \"" + s2 + "\" == 1 " + (add(s2) == 1));
@@ -93,5 +105,7 @@ public class StringCalculator
 
 		System.out.println("Testing \"" + s9 + "\" == 10 " + (add(s9) == 10));
 		System.out.println("Testing \"" + s10 + "\" == 1111 " + (add(s10) == 1111));
+		System.out.println("Testing \"" + s11 + "\" == 6 " + (add(s11) == 6));
+		System.out.println("Testing \"" + s12 + "\" == 290 " + (add(s12) == 290));
 	}
 }
